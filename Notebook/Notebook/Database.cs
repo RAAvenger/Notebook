c5992521bfd52c09bb482576aee2cbbd5f2ecdfb
Reza ADB <adbrezaooo@gmail.com>
2020-07-11 00:42:12 +0430

    Class database added, logIn and new use functions using hash Done!

---
+++ Notebook/Notebook/Database.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+
+namespace Notebook {
+    public class Database {
+        private SqlConnection connection;
+        /// <summary>
+        /// struct for Errors.
+        /// </summary>
+        enum ErrorMessage { required, dontMatchPattern, wrongPassword, wrongPasswordConfirmation, usernameAlredyExists };
+        struct Error {
+            public string FieldName;
+            public ErrorMessage Message;
+            public Error(string FieldName, ErrorMessage Message) {
+                this.FieldName = FieldName;
+                this.Message = Message;
+            }
+        }
+
+        public bool AddNewUser(string username, string password) {
+            try {
+                /// Hashing password.
+                byte[] salt;
+                new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+                var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000);
+                byte[] hash = pbkdf2.GetBytes(20);
+                byte[] hashBytes = new byte[36];
+                Array.Copy(salt, 0, hashBytes, 0, 16);
+                Array.Copy(hash, 0, hashBytes, 16, 20);
+                string hashedPassword = Convert.ToBase64String(hashBytes);
+                /// Add User to Database.
+                Connect();
+                SqlCommand command = new SqlCommand();
+                command.Connection = this.connection;
+                command.CommandText = "NewUser";
+                command.CommandType = CommandType.StoredProcedure;
+                SqlParameter param;
+                param = command.Parameters.Add("@username", SqlDbType.NVarChar, 25);
+                param.Value = username;
+                param = command.Parameters.Add("@password", SqlDbType.Text);
+                param.Value = hashedPassword;
+                command.ExecuteNonQuery();
+                Disconnect();
+                return true;
+            }
+            catch (Exception e) {
+                return false;
+            }
+        }
+
+        public bool UserValidation(string username, string password) {
+            try {
+                Connect();
+                //SqlCommand command = new SqlCommand("select true whare exists", this.connection);
+                SqlCommand command = new SqlCommand("SELECT * FROM Users where username = @username", this.connection);
+                command.Parameters.AddWithValue("@username", username);
+                SqlDataReader reader = command.ExecuteReader();
+                string hashedPasssword = null;
+                while (reader.Read()) {
+                    hashedPasssword = reader[2].ToString();
+                }
+                Disconnect();
+                /// compare password and hashed password.
+                if (!String.IsNullOrEmpty(hashedPasssword)) {
+                    byte[] hashBytes = Convert.FromBase64String(hashedPasssword);
+                    byte[] salt = new byte[16];
+                    Array.Copy(hashBytes, 0, salt, 0, 16);
+                    var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000);
+                    byte[] hash = pbkdf2.GetBytes(20);
+                    for (int i = 0; i < 20; i++)
+                        if (hashBytes[i + 16] != hash[i])
+                            return false;
+                    return true;
+                }
+                else
+                    return false;
+            }
+            catch (Exception e) {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// create and open Connection to database.
+        /// </summary>
+        private bool Connect() {
+            try {
+                this.connection = new SqlConnection("data source=.;initial catalog=Notebook;integrated security=True;");
+                this.connection.Open();
+                return true;
+            }
+            catch (Exception e) {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Close connection to database.
+        /// </summary>
+        private void Disconnect() {
+            this.connection.Close();
+        }
+    }
+}
--- Notebook/Notebook/LogIn_SignIn.xaml.cs
+++ Notebook/Notebook/LogIn_SignIn.xaml.cs
@@ -1,80 +1,250 @@
-﻿using System;
+﻿using Notebook;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-
+using System.Windows.Media;
 namespace Notbook {
     public partial class LogIn_SignIn : Window {
+        /// <summary>
+        /// variables.
+        /// </summary>
+        private bool isLogIn;
+        private List<Error> listErrors;
+        private Database database;
+        /// <summary>
+        /// struct for Errors.
+        /// </summary>
+        enum ErrorMessage { required, dontMatchPattern, wrongPassword, wrongPasswordConfirmation, usernameAlredyExists };
+        struct Error {
+            public string FieldName;
+            public ErrorMessage Message;
+            public Error(string FieldName, ErrorMessage Message) {
+                this.FieldName = FieldName;
+                this.Message = Message;
+            }
+        }
+
         public LogIn_SignIn() {
             InitializeComponent();
+            this.isLogIn = true;
             button_LogIn_Submit.IsEnabled = false;
-            button_SignIn_Submit.IsEnabled = false;
+            this.listErrors = new List<Error>();
+            this.listErrors.Add(new Error("LogIn_Username", ErrorMessage.required));
+            this.listErrors.Add(new Error("LogIn_Password", ErrorMessage.required));
+            this.database = new Database();
         }
         /// <summary>
         /// show Sign In Form and Hide Log In Form
         /// </summary>
         private void Button_SignIn_Click(object sender, RoutedEventArgs e) {
+            this.isLogIn = false;
             form_LogIn.Visibility = Visibility.Collapsed;
             form_SignIn.Visibility = Visibility.Visible;
             this.Title = "Sign In";
+            button_SignIn_Submit.IsEnabled = false;
+            this.listErrors.Clear();
+            this.listErrors.Add(new Error("SignIn_Username", ErrorMessage.required));
+            this.listErrors.Add(new Error("SignIn_Password", ErrorMessage.required));
+            this.listErrors.Add(new Error("SignIn_Confirmation", ErrorMessage.required));
         }
+
         /// <summary>
         /// submit Log In Data. 
         /// </summary>
         private void Button_LogIn_Submit_Click(object sender, RoutedEventArgs e) {
-            string username;
-            string password;
+           if( database.UserValidation(textBox_LogIn_Username.Text, passwordBox_LogIn_Password.Password))
+                MessageBox.Show("yes");
+           else
+                MessageBox.Show("no");
         }
+
         /// <summary>
         /// submit Sign In Data
         /// </summary>
         private void Button_SignIn_Submit_Click(object sender, RoutedEventArgs e) {
-
+            database.AddNewUser(textBox_SignIn_Username.Text, passwordBox_SignIn_Password.Password);
         }
+
         /// <summary>
         /// Log In input Validation
         /// </summary>
         private void TextBox_LogIn_Username_TextChanged(object sender, TextChangedEventArgs e) {
             if (String.IsNullOrEmpty(textBox_LogIn_Username.Text) || String.IsNullOrWhiteSpace(textBox_LogIn_Username.Text))
-                button_LogIn_Submit.IsEnabled = false;
-            else
-                button_LogIn_Submit.IsEnabled = true;
+                listErrors.Add(new Error("LogIn_Username", ErrorMessage.required));
+            else {
+                List<int> elementErrors = FindFieldErrorsIndexes(listErrors, "LogIn_Username");
+                for (int i = elementErrors.Count - 1; i >= 0; i--)
+                    listErrors.RemoveAt(elementErrors[i]);
+            }
+            ValidateForm();
         }
-
         private void PasswordBox_LogIn_Password_PasswordChanged(object sender, RoutedEventArgs e) {
             if (String.IsNullOrEmpty(passwordBox_LogIn_Password.Password) || String.IsNullOrWhiteSpace(passwordBox_LogIn_Password.Password))
-                button_LogIn_Submit.IsEnabled = false;
-            else
-                button_LogIn_Submit.IsEnabled = true;
+                listErrors.Add(new Error("LogIn_Password", ErrorMessage.required));
+            else {
+                List<int> elementErrors = FindFieldErrorsIndexes(listErrors, "LogIn_Password");
+                for (int i = elementErrors.Count - 1; i >= 0; i--)
+                    listErrors.RemoveAt(elementErrors[i]);
+            }
+            ValidateForm();
         }
+
         /// <summary>
         /// Sign In Input Validation
         /// </summary>
         private void TextBox_SignIn_Username_TextChanged(object sender, TextChangedEventArgs e) {
-            if (!(String.IsNullOrEmpty(textBox_SignIn_Username.Text) || String.IsNullOrWhiteSpace(textBox_SignIn_Username.Text))
-                && !(String.IsNullOrEmpty(passwordBox_SignIn_Password.Password) || String.IsNullOrWhiteSpace(passwordBox_SignIn_Password.Password))
-                && !(String.IsNullOrEmpty(passwordBox_SignIn_Confirmation.Password) || String.IsNullOrWhiteSpace(passwordBox_SignIn_Confirmation.Password)))
-                button_SignIn_Submit.IsEnabled = true;
-            else
-                button_SignIn_Submit.IsEnabled = false;
+            if (String.IsNullOrEmpty(textBox_SignIn_Username.Text) || String.IsNullOrWhiteSpace(textBox_SignIn_Username.Text))
+                listErrors.Add(new Error("SignIn_Username", ErrorMessage.required));
+            else {
+                List<int> elementErrors = FindFieldErrorsIndexes(listErrors, "SignIn_Username");
+                for (int i = elementErrors.Count - 1; i >= 0; i--)
+                    listErrors.RemoveAt(elementErrors[i]);
+            }
+            ValidateForm();
         }
-
         private void PasswordBox_SignIn_Password_PasswordChanged(object sender, RoutedEventArgs e) {
-            if ((String.IsNullOrEmpty(textBox_SignIn_Username.Text) || String.IsNullOrWhiteSpace(textBox_SignIn_Username.Text))
-                && (String.IsNullOrEmpty(passwordBox_SignIn_Password.Password) || String.IsNullOrWhiteSpace(passwordBox_SignIn_Password.Password))
-                && (String.IsNullOrEmpty(passwordBox_SignIn_Confirmation.Password) || String.IsNullOrWhiteSpace(passwordBox_SignIn_Confirmation.Password)))
-                button_SignIn_Submit.IsEnabled = false;
-            else
-                button_SignIn_Submit.IsEnabled = true;
+            if (String.IsNullOrEmpty(passwordBox_SignIn_Password.Password) || String.IsNullOrWhiteSpace(passwordBox_SignIn_Password.Password))
+                listErrors.Add(new Error("SignIn_Password", ErrorMessage.required));
+            else {
+                List<int> elementErrors = FindFieldErrorsIndexes(listErrors, "SignIn_Password");
+                for (int i = elementErrors.Count - 1; i >= 0; i--)
+                    listErrors.RemoveAt(elementErrors[i]);
+            }
+            ValidateForm();
+        }
+        private void PasswordBox_SignIn_Confirmation_PasswordChanged(object sender, RoutedEventArgs e) {
+            if (String.IsNullOrEmpty(passwordBox_SignIn_Confirmation.Password) || String.IsNullOrWhiteSpace(passwordBox_SignIn_Confirmation.Password))
+                listErrors.Add(new Error("SignIn_Confirmation", ErrorMessage.required));
+            else {
+                List<int> elementErrors = FindFieldErrorsIndexes(listErrors, "SignIn_Confirmation");
+                for (int i = elementErrors.Count - 1; i >= 0; i--)
+                    listErrors.RemoveAt(elementErrors[i]);
+            }
+            ValidateForm();
         }
 
-        private void PasswordBox_SignIn_Confirmation_PasswordChanged(object sender, RoutedEventArgs e) {
-            if ((String.IsNullOrEmpty(textBox_SignIn_Username.Text) || String.IsNullOrWhiteSpace(textBox_SignIn_Username.Text))
-                    && (String.IsNullOrEmpty(passwordBox_SignIn_Password.Password) || String.IsNullOrWhiteSpace(passwordBox_SignIn_Password.Password))
-                    && (String.IsNullOrEmpty(passwordBox_SignIn_Confirmation.Password) || String.IsNullOrWhiteSpace(passwordBox_SignIn_Confirmation.Password)))
-                button_SignIn_Submit.IsEnabled = false;
-            else
-                button_SignIn_Submit.IsEnabled = true;
+        /// <summary>
+        /// Validate form and Enable/UnEnable submit button.
+        /// Show invalid input fields.
+        /// </summary>
+        private void ValidateForm() {
+            ClearErrors();
+            if (listErrors.Count != 0) {
+                if (isLogIn)
+                    button_LogIn_Submit.IsEnabled = false;
+                else
+                    button_SignIn_Submit.IsEnabled = false;
+                foreach (Error error in listErrors) {
+                    string name = string.Concat("textBlock_", error.FieldName, "_Error_", error.Message.ToString());
+                    TextBlock textBlock_Error = new TextBlock() {
+                        Name = name,
+                        Foreground = Brushes.Red,
+                        FontSize = 12,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        TextAlignment = TextAlignment.Center,
+                        Width = Double.NaN,
+                        Height = Double.NaN
+                    };
+                    string errorText = null;
+                    int row = 5;
+                    switch (error.Message) {
+                        case ErrorMessage.required: {
+                            errorText = "فیلد بالا را پر کنید";
+                            row = 0;
+                            break;
+                        }
+                        case ErrorMessage.dontMatchPattern: {
+                            errorText = "مقدار ورودی با الگو تطابق ندارد";
+                            row = 1;
+                            break;
+                        }
+                        case ErrorMessage.wrongPassword: {
+                            errorText = "رمز ورود غلط است";
+                            row = 2;
+                            break;
+                        }
+                        case ErrorMessage.wrongPasswordConfirmation: {
+                            errorText = "رمز عبور و تکرار رمز عبور همخوانی ندارند";
+                            row = 3;
+                            break;
+                        }
+                        case ErrorMessage.usernameAlredyExists: {
+                            errorText = "نام‌کاربری موجود می‌باشد";
+                            row = 4;
+                            break;
+                        }
+                    }
+                    textBlock_Error.Text = String.IsNullOrEmpty(errorText) ? "خطا" : errorText;
+                    switch (error.FieldName) {
+                        case "LogIn_Username": {
+                            grid_LogIn_Username_Error.Children.Add(textBlock_Error);
+                            Grid.SetRow(textBlock_Error, row);
+                            break;
+                        }
+                        case "LogIn_Password": {
+                            grid_LogIn_Password_Error.Children.Add(textBlock_Error);
+                            Grid.SetRow(textBlock_Error, row);
+                            break;
+                        }
+                        case "SignIn_Username": {
+                            grid_SignIn_Username_Error.Children.Add(textBlock_Error);
+                            Grid.SetRow(textBlock_Error, row);
+                            break;
+                        }
+                        case "SignIn_Password": {
+                            grid_SignIn_Password_Error.Children.Add(textBlock_Error);
+                            Grid.SetRow(textBlock_Error, row);
+                            break;
+                        }
+                        case "SignIn_Confirmation": {
+                            grid_SignIn_Confirmation_Error.Children.Add(textBlock_Error);
+                            Grid.SetRow(textBlock_Error, row);
+                            break;
+                        }
+                    }
+                }
+            }
+            else {
+                if (isLogIn) {
+                    grid_LogIn_Username_Error.Children.Clear();
+                    grid_LogIn_Password_Error.Children.Clear();
+                    button_LogIn_Submit.IsEnabled = true;
+                }
+                else {
+                    grid_SignIn_Username_Error.Children.Clear();
+                    grid_SignIn_Password_Error.Children.Clear();
+                    grid_SignIn_Confirmation_Error.Children.Clear();
+                    button_SignIn_Submit.IsEnabled = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// find indexes of errors that blong to a form element.
+        /// </summary>
+        /// <returns>indexes of errors</returns>
+        private List<int> FindFieldErrorsIndexes(List<Error> errors, string fieldName) {
+            List<int> indexes = new List<int>();
+            for (int i = 0; i < errors.Count; i++)
+                if (errors[i].FieldName == fieldName)
+                    indexes.Add(i);
+            return indexes;
+        }
+
+        /// <summary>
+        /// clear Errors from errorGrids.
+        /// </summary>
+        private void ClearErrors() {
+            if (isLogIn) {
+                grid_LogIn_Username_Error.Children.Clear();
+                grid_LogIn_Password_Error.Children.Clear();
+            }
+            else {
+                grid_SignIn_Username_Error.Children.Clear();
+                grid_SignIn_Password_Error.Children.Clear();
+                grid_SignIn_Confirmation_Error.Children.Clear();
+            }
         }
     }
-
 }
