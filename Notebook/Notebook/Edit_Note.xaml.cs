32ca076915e9b4fe7dcb6fb7f626f0738380833b
Reza ADB <adbrezaooo@gmail.com>
2020-07-15 18:16:16 +0430

    Edit Note: Design Done!

--- Notebook/Notebook/Edit_Note.xaml.cs
+++ Notebook/Notebook/Edit_Note.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Notebook {
     /// <summary>
@@ -19,6 +7,10 @@ namespace Notebook {
     public partial class Edit_Note : Window {
         public Edit_Note() {
             InitializeComponent();
+        }
+
+        private void Button_OK_Click(object sender, RoutedEventArgs e) {
+            
         }
     }
 }
