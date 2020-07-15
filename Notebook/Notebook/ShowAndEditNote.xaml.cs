dedcd7aaae4930869e9a43cc1f260ef8b438061c
Reza ADB <adbrezaooo@gmail.com>
2020-07-15 23:49:41 +0430

    Merging 'Edit Note' and 'Show Note' Done!

---
+++ Notebook/Notebook/ShowAndEditNote.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace Notebook {
+    /// <summary>
+    /// Interaction logic for Edit_Note.xaml
+    /// </summary>
+    public partial class ShowAndEditNote : Window {
+        private bool newNoteFlage = false;
+        private bool noteTitleChangedFlag;
+        private bool noteTextChangedFlag;
+        private string noteTitle;
+        private string noteText;
+        public ShowAndEditNote() {
+            InitializeComponent();
+            this.noteTextChangedFlag = false;
+            this.noteTitleChangedFlag = false;
+            CheckNoteChanges();
+        }
+
+        #region titleBar Buttons
+        /// <summary>
+        /// Close Window.
+        /// </summary>
+        private void Button_Close_Click(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+        /// <summary>
+        /// back to prev Page. 
+        /// </summary>
+        private void Button_Back_Click(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+
+        #endregion titleBar Buttons
+        #region Page Buttons
+        /// <summary>
+        /// save changes.
+        /// </summary>
+        private void Button_OK_Click(object sender, RoutedEventArgs e) {
+            this.noteTextChangedFlag = false;
+            this.noteTitleChangedFlag = false;
+            this.noteTitle = textBox_Title.Text;
+            this.noteText = GetTextFromRichTextBox(richTextBox_Note);
+            CheckNoteChanges();
+        }
+        /// <summary>
+        /// discard changes.
+        /// </summary>
+        private void Button_Cancel_Click(object sender, RoutedEventArgs e) {
+            textBox_Title.Text = this.noteTitle;
+            richTextBox_Note.Document.Blocks.Clear();
+            richTextBox_Note.Document.Blocks.Add(new Paragraph(new Run(this.noteText)));
+            this.noteTextChangedFlag = false;
+            this.noteTitleChangedFlag = false;
+            CheckNoteChanges();
+        }
+        #endregion Page Buttons
+        #region TextChanged Functions
+        private void TextBox_Title_TextChanged(object sender, TextChangedEventArgs e) {
+            if (String.Compare(this.noteTitle, textBox_Title.Text) != 0) {
+                this.noteTitleChangedFlag = true;
+            }
+            else {
+                this.noteTitleChangedFlag = false;
+            }
+            CheckNoteChanges();
+        }
+
+        private void RichTextBox_Edit_Note_TextChanged(object sender, TextChangedEventArgs e) {
+            if (String.Compare(this.noteText, GetTextFromRichTextBox(richTextBox_Note)) != 0) {
+                this.noteTextChangedFlag = true;
+            }
+            else {
+                this.noteTextChangedFlag = false;
+            }
+            CheckNoteChanges();
+        }
+        #endregion TextChanged Functions
+        #region User Functions
+        /// <summary>
+        /// get text from rich text box.
+        /// </summary>
+        /// <param name="richtextbox"> the rich text box that you want its text</param>
+        /// <returns> rich text box's text as string </returns>
+        private string GetTextFromRichTextBox(RichTextBox richtextbox) {
+            try {
+                TextRange textRange = new TextRange(richtextbox.Document.ContentStart, richtextbox.Document.ContentEnd);
+                return textRange.Text;
+            }
+            catch (Exception e) {
+                return null;
+            }
+        }
+
+        private void CheckNoteChanges() {
+            try {
+                if (!this.noteTitleChangedFlag && !noteTextChangedFlag) {
+                    button_OK.IsEnabled = false;
+                    button_OK.Visibility = Visibility.Hidden;
+                    button_Cancel.IsEnabled = false;
+                    button_Cancel.Visibility = Visibility.Hidden;
+                }
+                else {
+                    button_OK.IsEnabled = true;
+                    button_OK.Visibility = Visibility.Visible;
+                    button_Cancel.IsEnabled = true;
+                    button_Cancel.Visibility = Visibility.Visible;
+                }
+            }
+            catch (Exception e) { }
+        }
+        #endregion User Functions
+
+    }
+}
--- Notebook/Notebook/Window1.xaml.cs
+++ Notebook/Notebook/Window1.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace Notebook {
     /// <summary>
@@ -8,5 +9,54 @@ namespace Notebook {
         public Window1() {
             InitializeComponent();
         }
+
+        ///// <summary>
+        ///// TitleBar_MouseDown - Drag if single-click, resize if double-click
+        ///// </summary>
+        //private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e) {
+        //    if (e.ChangedButton == MouseButton.Left)
+        //        if (e.ClickCount == 2) {
+        //            AdjustWindowSize();
+        //        }
+        //        else {
+        //            Application.Current.MainWindow.DragMove();
+        //        }
+        //}
+
+        ///// <summary>
+        ///// CloseButton_Clicked
+        ///// </summary>
+        //private void CloseButton_Click(object sender, RoutedEventArgs e) {
+        //    Application.Current.Shutdown();
+        //}
+
+        ///// <summary>
+        ///// MaximizedButton_Clicked
+        ///// </summary>
+        //private void MaximizeButton_Click(object sender, RoutedEventArgs e) {
+        //    AdjustWindowSize();
+        //}
+
+        ///// <summary>
+        ///// Minimized Button_Clicked
+        ///// </summary>
+        //private void MinimizeButton_Click(object sender, RoutedEventArgs e) {
+        //    this.WindowState = WindowState.Minimized;
+        //}
+
+        ///// <summary>
+        ///// Adjusts the WindowSize to correct parameters when Maximize button is clicked
+        ///// </summary>
+        //private void AdjustWindowSize() {
+        //    if (this.WindowState == WindowState.Maximized) {
+        //        this.WindowState = WindowState.Normal;
+                
+        //    }
+        //    else {
+        //        this.WindowState = WindowState.Maximized;
+        //    }
+
+        //}
+        
     }
 }
